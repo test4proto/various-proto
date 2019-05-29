b6f731e20c0288029c626ca882b3e11a9d4f4f81
test4proto <m2absolute_2@dyym.sakura.ne.jp>
2019-05-29 15:58:47 +0900

    Portal追加。

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
 import "./index.html";
+import AppRouter from "./AppRouter";
+
+library.add(fas);
 
 const app = document.querySelector("#app");
 
 if (app) {
-    render(<App />, app);
+    render(<AppRouter />, app);
 }
